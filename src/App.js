25491cf19a3e9d79e58ca310473978160225d094
Aboelela <eng.a.aboelela@gmail.com>
2023-06-20 11:14:42 +0300

    Toggle View visibility
    - Toggle visibility of items based on button click and state boolean value
    - Use Ternary condition
    - Change Button style

--- src/App.js
+++ src/App.js
@@ -10,28 +10,51 @@ class App extends Component {
       {name: 'Ahmed', age: 39},
       {name: 'Mido', age: 26},
       {name: 'Hossam', age: 37}
-    ]
+    ],
+    showPersons: false
   };
+
+  toggleVisibilityHandler = () => {
+    console.log('toggleVisibilityHandler Clicked!');
+    
+    this.setState({showPersons: !this.state.showPersons});
+  }
 
   render = () => {
     return (
       <div className="App">
         <p>
-          <button>Toggle View!</button>
+          <button className='button'
+            onClick={this.toggleVisibilityHandler}>
+              Toggle View Visibility!
+          </button>
         </p>
-        <Person 
-          name={this.state.personsData[0].name}
-          age={this.state.personsData[0].age}/>
+
+        {/*Add ternary condition statement 
+        to show/hide items */} 
         
-        <Person  
-          name={this.state.personsData[1].name}
-          age={this.state.personsData[1].age}/>
-        
-        <Person  
-          name={this.state.personsData[2].name}
-          age={this.state.personsData[2].age}>
-          I invest in my Happiness!
-        </Person>
+        {
+          this.state.showPersons ?
+          <div>
+            <Person 
+              name={this.state.personsData[0].name}
+              age={this.state.personsData[0].age}/>
+            
+            <Person  
+              name={this.state.personsData[1].name}
+              age={this.state.personsData[1].age}/>
+            
+            <Person  
+              name={this.state.personsData[2].name}
+              age={this.state.personsData[2].age}>
+              I invest in my Happiness!
+            </Person>
+          </div> :
+          <div>
+            Hidden contents
+          </div>
+        }
+
       </div>
     );
   }
